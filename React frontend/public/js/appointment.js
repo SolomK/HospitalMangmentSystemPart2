3e1fcc69f53a0203060116d43f2122ccae59fb14
kidus-yohannes1 <se.kidus.yohannes@gmail.com>
2021-06-17 18:35:36 +0300

    front end

---
+++ React frontend/public/js/appointment.js
@@ -0,0 +1,20 @@
+
+
+// var pname = document.getElementById("name");
+// var emaill = document.getElementById("email");
+
+// var dBirth = document.getElementById("dBirth");
+// var address = document.getElementById("address");
+// var pNumber = document.getElementById("pNumber");
+// var symptom = document.getElementById("symptom");
+// var uName = document.getElementById("uName");
+
+// var password = document.getElementById("password");
+// console.log(pname)
+// console.log(emaill)
+
+// var form = document.getElementById('form')
+// form.addEventListener('submit',function(e){
+    
+
+// })
